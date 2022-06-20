7e683fea6926864b7ba1faea900b2f0df9715453
Arthur <arthurvitorgn@gmail.com>
2022-06-20 12:55:26 -0300

    Better nested index

--- funcs.py
+++ funcs.py
@@ -16,12 +16,6 @@ def show_register(agenda, numero):
             return f'Nome: {agenda[c][0]}, Numero: {agenda[c][1]}'
 
 def NestedIndex(termo, agenda):
-    for c in range(0, len(agenda)): #Run each list
-        for k in range(0, len(agenda[c])): #Run each term in the lisk 'c'
-            if agenda[c][k] == termo: #Check if the term is the specified term
-                ListPos, TermPos = c, k
-                return ListPos, TermPos
-
-            #Utilization example, is this the most efficient way to do this? Offcourse no, but it will help
-            #nums = NestedIndex(teste(termo, agenda))
-            #print(lista[nums[0]][nums[1]])
+    for sub_lista in agenda:
+        if termo in sub_lista:
+            return agenda.index(sub_lista), sub_lista.index(termo)
--- main.py
+++ main.py
@@ -1,30 +1,14 @@
-import funcs
+import mysql.connector
 
-perguntas = {
-    1:'Agendar',
-    2:'Cancelar',
-    3:'Confirmar',
-    4:'Mostrar Registrados'
-}
-agendados = []
-esl = ''
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="",
+    database="chatbot"
+)
 
-for c in perguntas.keys(): #Mostra as opções
-    print(f'{c}: {perguntas[c]}')
+mycursor = mydb.cursor()
+mycursor.execute("SHOW TABLES")
 
-while esl != 5:
-    esl = int(input('Escolha uma das opções: '))
-    if esl == 1:
-        agendados.append(str(input('Precisaremos de alguns dados seus, digite seu nome e numero separadamente: ')).split())
-
-    elif esl == 2:
-        funcs.cancel(agendados, numero=str(input('Digite seu numero: ')))
-###dwadwa
-    elif esl == 3:
-        num = str(input('Digite seu numero: '))
-        sit = str(input('Qual sua situação?:  '))
-        funcs.confirmation(agendados, num, sit)
-
-    elif esl == 4:
-        num = str(input('Digite seu numero para verificar o registro: ')).strip()
-        print(funcs.show_register(agendados, num))
+for tb in mycursor: #Show all chatbot tables
+    print(tb)
