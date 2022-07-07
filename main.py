598ece87ba28c72ef8ceb2bc621e9c6df38534ce
Arthur <arthurvitorgn@gmail.com>
2022-07-07 16:57:38 -0300

    Python connection with mysql

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 from idlelib.debugobj import myrepr
-
 import mysql.connector, random as rd
 
-mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    passwd="",
-    database="chatbot"
-)
 
-def read_file(file):
-    x = open(file,'r', encoding = 'utf-8')
-    y = x.read()
-    content = y.splitlines()
-    return content
+# mydb = mysql.connector.connect(
+#     host="localhost",
+#     user="root",
+#     passwd="",
+#     database="chatbot"
+# )
 
-chat = read_file('Chats/Conversa do WhatsApp com Ravel.txt')
-print(len(chat))
+nums = [0,0]
+f = open('horas.txt', 'w')
+for c in range(0, 48):
+    f.write(f'{nums[0]}:{nums[1]} \n')
+    nums[1] += 30
+    if nums[1] == 60:
+        nums[0] += 1
+        nums[1] = 0
+f.close()
